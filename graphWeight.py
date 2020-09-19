bc0ef24569f23fd6b3a5a7d1610eb73a9c61126e
Adam M Jensiki <adam.jeniski@ibm.com>
2020-09-19 19:46:25 -0400

    init graphWeight.py

---
+++ graphWeight.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import pandas as pd
+from datetime import datetime
+
+df = pd.read_csv('Weight.csv')
+
+fig, ax = plt.subplots()
+
+dates = [datetime.strptime(date, '%m/%d/%y') for date in df["Date"].values]
+
+print(dates)
+
+weights = df["Weight"].values
+
+plt.plot(dates, weights)
+
+ax.set(xlabel='Time (date)', ylabel='Weight (lbs)',
+       title='Weight vs Time')
+
+fig.canvas.set_window_title('Weight Graph') 
+
+ax.xaxis.set_major_formatter(mdates.DateFormatter('%D'))
+
+fig.autofmt_xdate()
+
+plt.show()
